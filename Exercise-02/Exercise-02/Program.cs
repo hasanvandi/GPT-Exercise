af506b35f7df2ec3fa6a0ee71b2029a0fbb8b1f2
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-09-15 13:50:56 +0330

    exercise-02 added in gpt.exercise

---
+++ Exercise-02/Exercise-02/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Please Enter number in range 0 to 20:");
+int number = int.Parse(Console.ReadLine());
+
+
+
+if(number > 20 || number < 0)
+{
+    Console.WriteLine("Please enter the correct range.");
+}
+else if (number < 10)
+{
+    Console.WriteLine("Rejected");
+}
+else if (number > 10 && number <= 14)
+{
+    Console.WriteLine("Conditional acceptance.");
+}
+else
+{
+    Console.WriteLine("Successfully accepted");
+}
+
+
+Console.ReadLine();
