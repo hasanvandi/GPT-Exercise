60df772ffa54e57367bb2ca6a7d77d04744a30fb
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-10-30 10:32:55 +0330

    Exercise-05 added

---
+++ Exercise-05/Exercise-05/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Exercise_05;
+public class BankAccount
+{
+    private string AccountNumber;
+    private string OwnerName;
+    private decimal Balance;
+
+
+    public BankAccount()
+    {
+        AccountNumber = "00000000";
+        OwnerName = "null";
+        Balance = 0;
+    }
+
+
+    public BankAccount(string accountNumber, string ownerName, decimal balance)
+    {
+        AccountNumber = accountNumber;
+        OwnerName = ownerName;
+        Balance = balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            Console.WriteLine("Deposit completed!");
+        }
+        else
+        {
+            Console.WriteLine("The amount is invalid.");
+        }
+    }
+    public void Withdraw(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance -= amount;
+            Console.WriteLine("The withdrawal is done!");
+        }
+        else if (amount < Balance)
+        {
+            Console.WriteLine("Insufficient inventory!");
+        }
+        else
+        {
+            Console.WriteLine("The amount is invalid.");
+        }
+    }
+    public decimal GetBalance()
+    {
+        return Balance;
+    }
+
+    public void DisplayInfo()
+    {
+      Console.WriteLine($"AccountNumber:{AccountNumber} OwnerName:{OwnerName} Balance:{Balance}");
+    }
+}
+
---
+++ Exercise-05/Exercise-05/Program.cs
@@ -0,0 +1,26 @@
+﻿using Exercise_05;
+
+BankAccount bankAccount = new("0098732342354","MahdiHasanvandi",1000);
+BankAccount bankAccount2 = new();
+
+bankAccount.Deposit(55);
+decimal myBalance = bankAccount.GetBalance();
+Console.WriteLine("Your account balance is:" + myBalance);
+
+bankAccount2.Deposit(100);
+decimal myBalance2 = bankAccount2.GetBalance();
+Console.WriteLine("Your account balance is:" + myBalance2);
+
+bankAccount.Withdraw(20);
+decimal myBalance1 = bankAccount.GetBalance();
+Console.WriteLine($"Your account balance is:{myBalance1}");
+
+
+bankAccount2.Withdraw(30);
+decimal myBalance3 = bankAccount2.GetBalance();
+Console.WriteLine("Your account balance is:" + myBalance3);
+
+bankAccount.DisplayInfo();
+bankAccount2.DisplayInfo();
+
+Console.ReadLine();
